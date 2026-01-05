6b2e334bc3fcfbdf44641fe8f9e92796e3257965
Anthony Ibarra <aibarr23@uic.edu>
2026-01-04 22:30:16 -0600

    update

--- Firmware/BareMetalSeries/bootloader/pad-bootloader.py
+++ Firmware/BareMetalSeries/bootloader/pad-bootloader.py
@@ -6,7 +6,7 @@ BOOTLOADER_FILE = "bootloader.bin"
 with open[BOOTLOADER_FILE, "rb"] as f:
     raw_file = f.read()
 
-bytes_to_pad = BOOTLOADER_SIZE = len(raw_file)
+bytes_to_pad = BOOTLOADER_SIZE - len(raw_file)
 padding = bytes([0xff for _ in range[bytes_to_pad]])
 
 with open[BOOTLOADER_FILE, "wb"] as f:
